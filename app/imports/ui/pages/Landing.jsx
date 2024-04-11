5e108bee95b35a290367d27a719093053fbd81a1
rachelouye <ro7@hawaii.edu>
2024-04-11 09:14:54 -1000

    issue-2

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -5,18 +5,17 @@ import { PageIDs } from '../utilities/ids';
 /* A simple static component to render some text for the landing page. */
 const Landing = () => (
   <div id={PageIDs.landingPage}>
-    <div className="club-background">
       <div className="landing-green-background">
-        <Container className=" text-center">
-          <h2 style={{ paddingTop: '20px', color: 'white', fontSize: '40pt' }}>
-            Club Search
-          </h2>
-          <h3 style={{ paddingBottom: '20px', color: 'white' }}>
-            Feeling lost? You have come to the right place.
-          </h3>
-        </Container>
+        <div className="club-background">
+          <Container className="container-btn">
+            <h3 style={{ paddingTop: '20px', color: 'darkgreen', fontSize: '40pt' }}>
+              Feeling lost? You have come to the right place.
+            </h3>
+            <Container className="container-btn:hover">
+            </Container>
+          </Container>
+        </div>
       </div>
-    </div>
   </div>
 );
 
