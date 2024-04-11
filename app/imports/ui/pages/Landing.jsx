e8fe40299155bbea24ba5192f9d643023ae47ae5
rachelouye <ro7@hawaii.edu>
2024-04-10 17:02:13 -1000

    issue-2

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -15,46 +15,7 @@ const Landing = () => (
         </h3>
       </Container>
     </div>
-    <div className="landing-white-background">
-      <Container className="justify-content-center text-center">
-        <h2 style={{ color: '#376551' }}>Start by making your profile....</h2>
-        <Row md={1} lg={2}>
-          <Col xs={6}>
-            <Image src="/images/home-page.png" width={500} />
-          </Col>
-          <Col xs={6}>
-            <Image src="/images/profiles-page.png" width={500} />
-          </Col>
-        </Row>
-      </Container>
-    </div>
-    <div className="landing-green-background">
-      <Container className="justify-content-center text-center">
-        <h2 style={{ color: 'white' }}>...then add your projects</h2>
-        <Row md={1} lg={2}>
-          <Col xs={6}>
-            <Image src="/images/add-project-page.png" width={500} />
-          </Col>
-          <Col xs={6}>
-            <Image src="/images/projects-page.png" width={500} />
-          </Col>
-        </Row>
-      </Container>
-    </div>
-    <div className="landing-white-background text-center">
-      <h2 style={{ color: '#376551' }}>
-        Connect to people and projects with shared interests!
-      </h2>
-      <Container>
-        <Row md={1} lg={2}>
-          <Col xs={6}>
-            <Image src="/images/interests-page.png" width={500} />
-          </Col>
-          <Col xs={6}>
-            <Image src="/images/filter-page.png" width={500} />
-          </Col>
-        </Row>
-      </Container>
+    <div className="club-background">
     </div>
   </div>
 );
