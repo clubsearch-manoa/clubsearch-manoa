046df7c0707c736c2b25f5d58c3344183ae6a9a0
Sage Colón <shc2020@hawaii.edu>
2024-04-21 13:12:29 -1000

    Update Clubs.js to match issue-23 fixes

--- app/imports/api/clubs/Clubs.js
+++ app/imports/api/clubs/Clubs.js
@@ -3,22 +3,25 @@ import SimpleSchema from 'simpl-schema';
 
 /** Encapsulates state and variable values for this collection. */
 class ClubsCollection {
+
   constructor() {
     this.name = 'ClubsCollection';
     this.collection = new Mongo.Collection(this.name);
     this.schema = new SimpleSchema({
-    //  _id: String,
-      name: { type: String, unique: true },
-      image: { type: String, unique: true },
-      description: { type: String, unique: true },
-      meetingTimes: String,
-      contact: { type: String, unique: true },
-      tags: Array,
+      //  _id: String,
+      name: { type: String, unique: true, required: true, min: 1, max: 100 },
+      image: { type: String, unique: true, required: true, min: 1, max: 255 },
+      description: { type: String, unique: true, required: true, min: 1, max: 500 },
+      meetingTimes: { type: String, required: true, min: 1, max: 24 },
+      contact: { type: String, required: true, min: 1, max: 100 },
+      tags: { type: Array, required: true, min: 1, max: 6 },
       'tags.$': {
         type: String,
-        allowedValues: ['Religion', 'Honor Society', 'Martial Arts', 'Ethnic', 'Other'],
+        allowedValues: ['Religion', 'Honor Society', 'Martial Arts', 'Ethnic', 'Food', 'Other'],
+        min: 1,
+        max: 14,
       },
-    }.isRequired);
+    });
 
     this.collection.attachSchema(this.schema);
     this.userPublications = `${this.name}.publication.user`;
