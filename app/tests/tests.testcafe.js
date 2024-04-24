630de166c3cdcf9783a16d1ba9caf8af36d5db54
Tristan Yousuf-Leo <tyousufleo8@gmail.com>
2024-04-23 14:57:22 -1000

    FIXED THE CREDENTIALS LMAO

--- app/tests/tests.testcafe.js
+++ app/tests/tests.testcafe.js
@@ -15,7 +15,7 @@ import { deleteClubPage } from './deleteclub.page';
 /* global fixture:false, test:false */
 
 /** Credentials for one of the sample users defined in settings.development.json. */
-const credentials = { username: 'johnson@hawaii.edu', password: 'foo', firstName: 'Philip', lastName: 'Johnson' };
+const credentials = { username: 'superadmin@foo.com', password: 'changeme' };
 
 fixture('Bowfolios localhost test with default db')
   .page('http://localhost:3000');
@@ -42,24 +42,6 @@ test('Test that signup page, then logout works', async (testController) => {
   await signOutPage.isDisplayed(testController);
 });
 
-test('Test that profiles page displays', async (testController) => {
-  await navBar.gotoProfilesPage(testController);
-  await profilesPage.isDisplayed(testController);
-  await profilesPage.hasDefaultProfiles(testController);
-});
-
-test('Test that interests page displays', async (testController) => {
-  await navBar.gotoInterestsPage(testController);
-  await interestsPage.isDisplayed(testController);
-  await interestsPage.hasDefaultInterests(testController);
-});
-
-test('Test that projects page displays', async (testController) => {
-  await navBar.gotoProjectsPage(testController);
-  await projectsPage.isDisplayed(testController);
-  await projectsPage.hasDefaultProjects(testController);
-});
-
 test('Test that home page display and profile modification works', async (testController) => {
   await navBar.ensureLogout(testController);
   await navBar.gotoSignInPage(testController);
@@ -67,24 +49,6 @@ test('Test that home page display and profile modification works', async (testCo
   await homePage.isDisplayed(testController);
   await homePage.updateProfile(testController, credentials.firstName);
   await navBar.ensureLogout(testController);
-});
-
-test('Test that addProject page works', async (testController) => {
-  await navBar.ensureLogout(testController);
-  await navBar.gotoSignInPage(testController);
-  await signInPage.signin(testController, credentials.username, credentials.password);
-  await navBar.gotoAddProjectPage(testController);
-  await addProjectPage.isDisplayed(testController);
-  await addProjectPage.addProject(testController);
-});
-
-test('Test that filter page works', async (testController) => {
-  await navBar.ensureLogout(testController);
-  await navBar.gotoSignInPage(testController);
-  await signInPage.signin(testController, credentials.username, credentials.password);
-  await navBar.gotoFilterPage(testController);
-  await filterPage.isDisplayed(testController);
-  await filterPage.filter(testController);
 });
 
 test('Test that addClub page works', async (testController) => {
