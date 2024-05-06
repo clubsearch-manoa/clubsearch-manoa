311cf133cbe92457b25f088b131e35944c854732
cyrilra <cyrilra@hawaii.edu>
2024-05-05 16:52:39 -1000

    created and implemented a loading screen into landing page

--- app/imports/ui/layouts/App.jsx
+++ app/imports/ui/layouts/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Meteor } from 'meteor/meteor';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
@@ -19,34 +19,46 @@ import AddClub from '../pages/AddClub';
 import BrowseClubs from '../pages/BrowseClubs';
 import DeleteClub from '../pages/DeleteClub';
 import EditClub from '../pages/EditClub';
+import Loading from '../components/Loading';
 
 /* Top-level layout component for this application. Called in imports/startup/client/startup.jsx. */
-const App = () => (
-  <Router>
-    <div className="d-flex flex-column min-vh-100">
-      <NavBar />
-      <Routes>
-        <Route exact path="/" element={<Landing />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/signout" element={<SignOut />} />
-        <Route path="/browseclubs" element={<BrowseClubs />} />
-        <Route path="/addclub" element={<ProtectedRoute><AddClub /></ProtectedRoute>} />
-        <Route path="/deleteclub" element={<ProtectedRoute><DeleteClub /></ProtectedRoute>} />
-        <Route path="/editclub" element={<ProtectedRoute><EditClub /></ProtectedRoute>} />
-        <Route path="/interests" element={<Interests />} />
-        <Route path="/profiles" element={<Profiles />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/home" element={<ProtectedRoute><Home /></ProtectedRoute>} />
-        <Route path="/filter" element={<ProtectedRoute><Filter /></ProtectedRoute>} />
-        <Route path="/addproject" element={<ProtectedRoute><AddClub /></ProtectedRoute>} />
-        <Route path="/notauthorized" element={<NotAuthorized />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-      <Footer />
-    </div>
-  </Router>
-);
+const App = () => {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 3300);
+  }, []);
+  if (loading) {
+    // setTimeout(function(){debugger;}, 2000); use this for debugging and testing loading screen
+    return <Loading />;
+  }
+
+  return (
+    <Router>
+      <div className="d-flex flex-column min-vh-100">
+        <NavBar />
+        <Routes>
+          <Route exact path="/" element={<Landing />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/signout" element={<SignOut />} />
+          <Route path="/browseclubs" element={<BrowseClubs />} />
+          <Route path="/addclub" element={<ProtectedRoute><AddClub /></ProtectedRoute>} />
+          <Route path="/deleteclub" element={<ProtectedRoute><DeleteClub /></ProtectedRoute>} />
+          <Route path="/editclub" element={<ProtectedRoute><EditClub /></ProtectedRoute>} />
+          <Route path="/interests" element={<Interests />} />
+          <Route path="/profiles" element={<Profiles />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/home" element={<ProtectedRoute><Home /></ProtectedRoute>} />
+          <Route path="/filter" element={<ProtectedRoute><Filter /></ProtectedRoute>} />
+          <Route path="/addproject" element={<ProtectedRoute><AddClub /></ProtectedRoute>} />
+          <Route path="/notauthorized" element={<NotAuthorized />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
+};
 
 /*
  * ProtectedRoute (see React Router v6 sample)
