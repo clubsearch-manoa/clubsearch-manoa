43f528cc7a900a4f1dd48c1519e8b90b030725d9
rachelouye <ro7@hawaii.edu>
2024-05-08 17:39:38 -1000

    updated design of button on landing and directed it to go to browse clubs Updated login title to be more visible Changed titles on pages to be uniform Changed sign out font Added Landing title edited layout of everything to look uniform

--- app/imports/ui/pages/EditClub.jsx
+++ app/imports/ui/pages/EditClub.jsx
@@ -56,15 +56,19 @@ const EditClub = () => {
             <Card.Body>
               <Row>
                 <Col xs={4}><TextField id={ComponentIDs.editClubFormName} name="name" showInlineError placeholder="Club Name" /></Col>
+              </Row>
+              <Row>
                 <Col xs={6}><TextField id={ComponentIDs.editClubFormImage} name="image" showInlineError placeholder="URL to picture" /></Col>
               </Row>
               <LongTextField id={ComponentIDs.editClubFormDescription} name="description" placeholder="Write a little bit about your club." />
               <Row>
-                <Col xs={4}><SelectField name="tags" multiple checkboxes transform={transform} /></Col>
+                <Col xs={4}><TextField id={ComponentIDs.editClubFormMeetingTimes} name="meetingTimes" showInlineError placeholder="Meeting Times" /></Col>
+              </Row>
+              <Row>
                 <Col xs={4}><TextField id={ComponentIDs.editClubFormContact} name="contact" showInlineError placeholder="Contact" /></Col>
               </Row>
               <Row>
-                <Col xs={4}><TextField id={ComponentIDs.editClubFormMeetingTimes} name="meetingTimes" showInlineError placeholder="Meeting Times" /></Col>
+                <SelectField name="tags" multiple checkboxes transform={transform} />
               </Row>
               <SubmitField id={ComponentIDs.editClubFormSubmit} value="Update" />
             </Card.Body>
